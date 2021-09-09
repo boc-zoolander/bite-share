23d43c640b82a58d1c927658e87d6c1f6670fb40
jabrake <jorge.brake@gmail.com>
2021-09-08 20:33:41 -0700

    Added .js extension to file

---
+++ server/user_db_routes/testRestaurantsByZip_1.js
@@ -0,0 +1,141 @@
+
+var restaurantsByZip = {
+  "totalResults": 639,
+  "page": 1,
+  "total_pages": 128,
+  "more_pages": true,
+  "data": [
+      {
+          "restaurant_name": "Bakeri",
+          "restaurant_phone": "(718) 388-8037",
+          "restaurant_website": "",
+          "hours": "Daily: 8am-7pm",
+          "price_range": "$",
+          "price_range_num": 1,
+          "restaurant_id": 4072005273960112,
+          "cuisines": [
+              "Bakery & Pastries",
+              "Coffee & Tea",
+              "Sandwiches"
+          ],
+          "address": {
+              "city": "Brooklyn",
+              "state": "NY",
+              "postal_code": "11211",
+              "street": "150 Wythe Ave",
+              "formatted": "150 Wythe Ave Brooklyn, NY 11211"
+          },
+          "geo": {
+              "lat": 40.720052,
+              "lon": -73.960112
+          },
+          "menus": [],
+          "last_updated": "2021-01-05T07:36:14.169Z"
+      },
+      {
+          "restaurant_name": "Desy's Clam Bar",
+          "restaurant_phone": "(347) 599-0267",
+          "restaurant_website": "",
+          "hours": "Daily: 11am-12:30am",
+          "price_range": "$$$",
+          "price_range_num": 3,
+          "restaurant_id": 4071110873949341,
+          "cuisines": [
+              "Italian",
+              "Seafood"
+          ],
+          "address": {
+              "city": "Brooklyn",
+              "state": "NY",
+              "postal_code": "11211",
+              "street": "562 Grand St",
+              "formatted": "562 Grand St Brooklyn, NY 11211"
+          },
+          "geo": {
+              "lat": 40.711108,
+              "lon": -73.949341
+          },
+          "menus": [],
+          "last_updated": "2021-01-05T07:36:14.221Z"
+      },
+      {
+          "restaurant_name": "Tainas",
+          "restaurant_phone": "(718) 218-8890",
+          "restaurant_website": "",
+          "hours": "Mon-Thurs: 10am-10pm Fri: 10am-12am Sat: 12pm-12am Sun: 12pm-10pm",
+          "price_range": "$$",
+          "price_range_num": 2,
+          "restaurant_id": 4070795473955776,
+          "cuisines": [
+              "Latin American"
+          ],
+          "address": {
+              "city": "Brooklyn",
+              "state": "NY",
+              "postal_code": "11211",
+              "street": "347 Broadway",
+              "formatted": "347 Broadway Brooklyn, NY 11211"
+          },
+          "geo": {
+              "lat": 40.707954,
+              "lon": -73.955776
+          },
+          "menus": [],
+          "last_updated": "2021-01-05T07:36:14.245Z"
+      },
+      {
+          "restaurant_name": "Tender Trap",
+          "restaurant_phone": "(347) 763-1825",
+          "restaurant_website": "",
+          "hours": "Mon-Wed: 6pm-2am Thu-Sat: 4pm-4am Sun: 6pm-2am",
+          "price_range": "$",
+          "price_range_num": 1,
+          "restaurant_id": 4071262973957994,
+          "cuisines": [
+              "Other"
+          ],
+          "address": {
+              "city": "Brooklyn",
+              "state": "NY",
+              "postal_code": "11211",
+              "street": "245 S 1st St",
+              "formatted": "245 S 1st St Brooklyn, NY 11211"
+          },
+          "geo": {
+              "lat": 40.712629,
+              "lon": -73.957994
+          },
+          "menus": [],
+          "last_updated": "2021-01-05T07:36:14.889Z"
+      },
+      {
+          "restaurant_name": "Yenisse",
+          "restaurant_phone": "(718) 963-0934",
+          "restaurant_website": "",
+          "hours": "Daily: 9am-9pm",
+          "price_range": "$",
+          "price_range_num": 1,
+          "restaurant_id": 4071302073937186,
+          "cuisines": [
+              "Latin American",
+              "Puerto Rican"
+          ],
+          "address": {
+              "city": "Brooklyn",
+              "state": "NY",
+              "postal_code": "11211",
+              "street": "927 Grand St",
+              "formatted": "927 Grand St Brooklyn, NY 11211"
+          },
+          "geo": {
+              "lat": 40.71302,
+              "lon": -73.937186
+          },
+          "menus": [],
+          "last_updated": "2021-01-05T07:36:14.891Z"
+      }
+  ],
+  "numResults": 5
+}
+
+module.exports = restaurantsByZip;
