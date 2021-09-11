87b2ff1bf813d604a0d524951a34494f61655d0a
Michael Duckworth <michael.h.duckworth@gmail.com>
2021-09-11 14:00:49 -0400

    Cleaned up comments

--- client/src/components/SelectFood.jsx
+++ client/src/components/SelectFood.jsx
@@ -1,25 +1,9 @@
 import React, { useState } from 'react';
 // import PropTypes from 'prop-types';
 
-/**
- * Props: guests (array of guest object and current orders)
- *          {
- *            guestName: 'Person',
- *          }
- *        host
- *        menu
- *
- * Drop-down with guest names (and host name) from guests and host prop (mapping function). Select tags.
- * onChange handler  to set hook/state name
- */
-
 const SelectFood = ({ guests, host, menu, addToOrder }) => {
   const [currentName, setCurrentName] = useState('');
-  // const [currentOrder, setCurrentOrder] = useState([]);
 
-  // ADD USEEFFECT TO DISPLAY CURRENT ORDER (IN CASE USER RETURNS TO PERSON)
-
-  // MAP guest names, menu, and current order
   const guestNames = guests.map((item, i) => {
     return (
       <option key={i} value={item.guestName} > {item.guestName} </option>
@@ -38,8 +22,6 @@ const SelectFood = ({ guests, host, menu, addToOrder }) => {
               {item.price}
               <button type='button' onClick={() => { addItem(item); }}> + </button>
             </li>
-            // price
-            // add to order button
           );
         })}
         </ul>
@@ -60,22 +42,17 @@ const SelectFood = ({ guests, host, menu, addToOrder }) => {
   };
 
   const addItem = (item) => {
-    // const order = [...currentOrder];
-    // order.push(item);
-    // setCurrentOrder(order);
     addToOrder(currentName, item);
   };
 
   return (
     <div>
-      {/* <button type='button' onClick={addOrder}>Done</button> */}
       <label htmlFor='guestName' > Add items for </label>
       <select name='guestName' onChange={onChange}>
         <option value='' selected disabled hidden > Choose Guest </option>
         <option value={host.name}> {host.name} </option>
         {guestNames}
       </select>
-      {/* PLACEHOLDER FOR SELECTED ITEMS BEFORE PUSHING TO APP STATE (LOCAL HOOK) */}
       <h4> Current Items for {currentName} </h4>
         {currentItems}
       <div>
