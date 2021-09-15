05d28bbd8b78e08d9ce091e847ceb33b4a1f95f4
Sara Landis <saralandis5555@gmail.com>
2021-09-15 12:51:50 -0400

    More tests

--- tests/billSplittingTests.test.js
+++ tests/billSplittingTests.test.js
@@ -56,15 +56,69 @@ describe('Unit Test Section: <BillSummaryPage />', () => {
     );
   });
 
-  // test('Unit Test 2:  Does the component <BillSummaryPage /> contain the header Final Bill?', () => {
-  //   render(<BillSummaryPage />);
-  //   expect(screen.getByText(/Final Bill/)).toBeInTheDocument();
-  // });
+  test('Unit Test 2:  Does the component <BillSummaryPage /> contain the header Final Bill?', () => {
+    const guestsProps = [
+      {
+        id: 0,
+        guestName: 'Sara',
+        order: [
+          { name: 'Regular Coffee', description: '', pricing: [{ price: 2.25, currency: 'USD', priceString: '$2.25' }], price: 2.25 },
+          { name: 'Flap Jacks', description: '', pricing: [{ price: 7.75, currency: 'USD', priceString: '$7.75' }], price: 7.75 },
+          { name: 'El Cuban Reuben Panini', description: 'Roast pork, Swiss cheese, braised red cabbage and mustard on ciabatta.', pricing: [{ price: 14.75, currency: 'USD', priceString: '$14.75' }], price: 14.75 }
+        ]
+      },
+      {
+        guestName: 'Milo',
+        order: [
+          { name: 'Regular Coffee', description: '', pricing: [{ price: 2.25, currency: 'USD', priceString: '$2.25' }], price: 2.25 },
+          { name: 'Flap Jacks', description: '', pricing: [{ price: 7.75, currency: 'USD', priceString: '$7.75' }], price: 7.75 },
+          { name: 'El Cuban Reuben Panini', description: 'Roast pork, Swiss cheese, braised red cabbage and mustard on ciabatta.', pricing: [{ price: 14.75, currency: 'USD', priceString: '$14.75' }], price: 14.75 }
+        ]
+      },
+      {
+        guestName: 'Mike',
+        order: [
+          { name: 'Regular Coffee', description: '', pricing: [{ price: 2.25, currency: 'USD', priceString: '$2.25' }], price: 2.25 },
+          { name: 'Flap Jacks', description: '', pricing: [{ price: 7.75, currency: 'USD', priceString: '$7.75' }], price: 7.75 },
+          { name: 'El Cuban Reuben Panini', description: 'Roast pork, Swiss cheese, braised red cabbage and mustard on ciabatta.', pricing: [{ price: 14.75, currency: 'USD', priceString: '$14.75' }], price: 14.75 }
+        ]
+      }
+    ];
+    render(<BrowserRouter><BillSummaryPage guests = {guestsProps}/></BrowserRouter>);
+    expect(screen.getByText(/Final Bill/)).toBeInTheDocument();
+  });
 
-  // test('Unit Test 3:  Does the component <BillSummaryPage /> contain the Total', () => {
-  //   render(<BillSummaryPage />);
-  //   expect(screen.getByText(/Total/)).toBeInTheDocument();
-  // });
+  test('Unit Test 3:  Does the component <BillSummaryPage /> contain the Total', () => {
+    const guestsProps = [
+      {
+        id: 0,
+        guestName: 'Sara',
+        order: [
+          { name: 'Regular Coffee', description: '', pricing: [{ price: 2.25, currency: 'USD', priceString: '$2.25' }], price: 2.25 },
+          { name: 'Flap Jacks', description: '', pricing: [{ price: 7.75, currency: 'USD', priceString: '$7.75' }], price: 7.75 },
+          { name: 'El Cuban Reuben Panini', description: 'Roast pork, Swiss cheese, braised red cabbage and mustard on ciabatta.', pricing: [{ price: 14.75, currency: 'USD', priceString: '$14.75' }], price: 14.75 }
+        ]
+      },
+      {
+        guestName: 'Milo',
+        order: [
+          { name: 'Regular Coffee', description: '', pricing: [{ price: 2.25, currency: 'USD', priceString: '$2.25' }], price: 2.25 },
+          { name: 'Flap Jacks', description: '', pricing: [{ price: 7.75, currency: 'USD', priceString: '$7.75' }], price: 7.75 },
+          { name: 'El Cuban Reuben Panini', description: 'Roast pork, Swiss cheese, braised red cabbage and mustard on ciabatta.', pricing: [{ price: 14.75, currency: 'USD', priceString: '$14.75' }], price: 14.75 }
+        ]
+      },
+      {
+        guestName: 'Mike',
+        order: [
+          { name: 'Regular Coffee', description: '', pricing: [{ price: 2.25, currency: 'USD', priceString: '$2.25' }], price: 2.25 },
+          { name: 'Flap Jacks', description: '', pricing: [{ price: 7.75, currency: 'USD', priceString: '$7.75' }], price: 7.75 },
+          { name: 'El Cuban Reuben Panini', description: 'Roast pork, Swiss cheese, braised red cabbage and mustard on ciabatta.', pricing: [{ price: 14.75, currency: 'USD', priceString: '$14.75' }], price: 14.75 }
+        ]
+      }
+    ];
+    render(<BrowserRouter><BillSummaryPage guests = {guestsProps}/></BrowserRouter>);
+    expect(screen.getByText(/Total/)).toBeInTheDocument();
+  });
 
   // test('Unit Test 4:  Does the component <BillSummaryPage /> allow user to change split method to Split evenly', () => {
   //   render(<BillSummaryPage />);
