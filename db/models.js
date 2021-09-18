9323d06bb22034f459eee5921f9d4cecb87c60a9
Milo Fultz <milopfultz@gmail.com>
2021-09-17 18:26:12 -0700

    Update ENV for overwriting

--- db/models.js
+++ db/models.js
@@ -6,7 +6,7 @@ const pool = new Pool({
   host: process.env.HOST,
   database: process.env.DATABASE_NAME,
   password: process.env.PASSWORD,
-  port: process.env.PORT,
+  port: process.env.PORT
 });
 
 pool.connect();
