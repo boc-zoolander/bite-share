28f8d61a81be875d18cc4941661440373f067d4b
Milo Fultz <milopfultz@gmail.com>
2021-09-17 17:33:22 -0700

    Get it rendering

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import Search from './components/Search.jsx';
 import Join from './components/Join.jsx';
 import GuestMenu from './components/GuestMenu.jsx';
 import HostMenu from './components/HostMenu.jsx';
+import Session from './components/Session.jsx';
 
 class App extends React.Component {
   constructor (props) {
@@ -36,23 +37,26 @@ class App extends React.Component {
       <div>
       <Router>
         <Switch>
-          <Route path="/find-restaurant">
-            <Search setTopLevelState={this.setTopLevelState} hostGeo={this.state.hostGeo} hostZipCode={this.state.hostZipCode} />
-          </Route>
           <Route path="/add-guests">
             <AddGuests setTopLevelState={this.setTopLevelState} guests={this.state.guests} />
           </Route>
-          <Route path="/join">
-            <Join setTopLevelState={this.setTopLevelState}/>
+          <Route path="/find-restaurant">
+            <Search setTopLevelState={this.setTopLevelState} hostGeo={this.state.hostGeo} hostZipCode={this.state.hostZipCode} />
           </Route>
           <Route path="/guest-menu">
             <GuestMenu joinName={this.state.joinName}/>
+          </Route>
+          <Route path="/join">
+            <Join setTopLevelState={this.setTopLevelState}/>
           </Route>
           <Route path="/host-menu">
             <HostMenu setTopLevelState={this.setTopLevelState} guests={this.state.guests} menu={this.state.menu} />
           </Route>
           <Route path="/select-food">
             <SelectFood setTopLevelState={this.setTopLevelState} guests={this.state.guests} menu={this.state.menu} />
+          </Route>
+          <Route path="/session">
+            <Session setTopLevelState={this.setTopLevelState} />
           </Route>
           <Route path="/split-bill">
             <BillSummaryPage setTopLevelState={this.setTopLevelState} guests={this.state.guests} zipCode= {this.state.restaurantZipCode} />
