874129ed5ac9f45e5517cabad27c41233dd0d716
Milo Fultz <milopfultz@gmail.com>
2021-09-17 17:50:46 -0700

    Make basic mockup

--- client/src/components/Session.jsx
+++ client/src/components/Session.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Session () {
+  const [message, setMessage] = useState('');
+
   return (
-    <h1>It works!</h1>
+    <div>
+      <h2>Session</h2>
+      <button type="button">Start a New Session</button>
+      {message && <p>{message}</p>}
+      <form>
+        <label htmlFor="session-id">Enter an Existing Session</label>
+        <input type="number" name="session-id" />
+        <button type="button" onClick={() => setMessage('Test complete')}>Submit</button>
+      </form>
+    </div>
   );
 }
