ff632438d4e4de68782f769b833dd603d4def91a
Milo Fultz <milopfultz@gmail.com>
2021-09-17 18:31:24 -0700

    Update env names in models

--- db/models.js
+++ db/models.js
@@ -1,12 +1,12 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: process.env.USER,
+  user: process.env.POSTGRES_USER,
   // name of the EC2 server IP
-  host: process.env.HOST,
-  database: process.env.DATABASE_NAME,
-  password: process.env.PASSWORD,
-  port: process.env.PORT
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DATABASE_NAME,
+  password: process.env.POSTGRES_PASSWORD,
+  port: process.env.POSTGRES_PORT
 });
 
 pool.connect();
