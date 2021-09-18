27053a5b3b7659f81baea80f061dd86078ed0dec
Milo Fultz <milopfultz@gmail.com>
2021-09-17 21:44:38 -0700

    Add join session functionality with mock data

--- client/src/components/Session.jsx
+++ client/src/components/Session.jsx
@@ -1,22 +1,67 @@
+import axios from 'axios';
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
-export default function Session () {
-  const [message, setMessage] = useState('');
+export default function Session ({ setTopLevelState }) {
+  const history = useHistory();
+  const [sessionId, setSessionId] = useState('');
+  const [loadingSession, setLoadingSession] = useState(false);
+
+  const handleSessionIdChange = (e) => {
+    setSessionId(e.target.value);
+  };
+
+  const handleJoinSession = async () => {
+    // set loading to true
+    setLoadingSession(true);
+    // make axios call for session object
+    const sessionResponse = await axios('/users/getSession');
+    // update host in state
+    // update guests in state
+    const allGuests = [
+      sessionResponse.data.host,
+      ...sessionResponse.data.guests
+    ];
+    setTopLevelState('guests', allGuests);
+    // load restaurant data via restaurant api
+    // update restaurant in state
+    // set loading to false
+    setLoadingSession(false);
+    // redirect to next page
+    history.push('/select-food');
+  };
+
+  const content = () => {
+    if (loadingSession) {
+      return (
+        <p>Loading session...</p>
+      );
+    } else {
+      return (
+        <>
+          {/* Will eventually go to "Host a Session" component */}
+          <Link to="/find-restaurant">
+            <button type="button">Start a New Session</button>
+          </Link>
+          <form>
+            <label htmlFor="session-id">Enter an Existing Session</label>
+            <input
+              type="number"
+              name="session-id"
+              value={sessionId}
+              onChange={handleSessionIdChange}
+            />
+            <button type="button" onClick={handleJoinSession}>Submit</button>
+          </form>
+        </>
+      );
+    }
+  };
 
   return (
     <div>
       <h2>Session</h2>
-      {/* Will eventually go to "Host a Session" component */}
-      <Link to="/find-restaurant">
-        <button type="button">Start a New Session</button>
-      </Link>
-      {message && <p>{message}</p>}
-      <form>
-        <label htmlFor="session-id">Enter an Existing Session</label>
-        <input type="number" name="session-id" />
-        <button type="button" onClick={() => setMessage('Test complete')}>Submit</button>
-      </form>
+      {content()}
     </div>
   );
 }
--- db/models.js
+++ db/models.js
@@ -3,17 +3,10 @@ const { Pool } = require('pg');
 const pool = new Pool({
   user: process.env.POSTGRES_USER,
   // name of the EC2 server IP
-<<<<<<< Updated upstream
   host: process.env.POSTGRES_HOST,
   database: process.env.POSTGRES_DATABASE_NAME,
   password: process.env.POSTGRES_PASSWORD,
   port: process.env.POSTGRES_PORT
-=======
-  host: process.env.HOST,
-  database: process.env.DATABASE_NAME,
-  password: process.env.PASSWORD,
-  port: process.env.PORT
->>>>>>> Stashed changes
 });
 
 pool.connect();
