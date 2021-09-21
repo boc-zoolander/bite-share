ffe70c4f9e69c60502bea009d5d8d7002f7f2609
jabrake <jorge.brake@gmail.com>
2021-09-20 20:55:47 -0700

    Wrote more unit tests

--- tests/CreateSession.test.js
+++ tests/CreateSession.test.js
@@ -1,15 +1,36 @@
+import axios from 'axios';
 import { render, screen, cleanup, fireEvent, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
 // import { configure, shallow, mount } from 'enzyme';
 // import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import CreateSession from '../client/src/components/CreateSession.jsx';
+const testRestaurantsByGeo = require('../server/user_db_routes/testRestaurantsByGeo');
+const testRestaurantsByZip = require('../server/user_db_routes/testRestaurantsByZip_1');
+
+jest.mock('axios');
 
 describe('<CreateSession />', () => {
+  const restaurantData = testRestaurantsByGeo;
+  axios.get.mockImplementation(() => new Promise((resolve, reject) => {
+    resolve({
+      data: restaurantData
+    });
+  }));
+
   beforeEach(() => {
+    const history = createMemoryHistory();
     render(
-      <CreateSession />
+      <Router history={history}>
+        <Route path="/add-guests">
+          <p>Create your guest list</p>
+        </Route>
+        <Route exact path="/">
+          <CreateSession />
+        </Route>
+      </Router>
     );
   });
 
@@ -33,9 +54,67 @@ describe('<CreateSession />', () => {
     const input = screen.getByTestId('session-name-input');
     fireEvent.change(input, { target: { value: 'Portland Dinner' } });
     const saveSessionNameButton = screen.getByText('Save');
-    // const leftClick = { button: 0 };
     fireEvent.click(saveSessionNameButton, { button: 0 });
     const sessionName = await screen.getByText('Session Name: Portland Dinner');
     expect(sessionName).toBeInTheDocument();
+  });
+
+  it('should load suggested restaurants after saving session name', async () => {
+    expect(axios.get).toHaveBeenCalled();
+
+    const sessionNameInput = screen.getByTestId('session-name-input');
+    fireEvent.change(sessionNameInput, { target: { value: 'Portland Dinner' } });
+    const saveSessionNameButton = screen.getByText('Save');
+    fireEvent.click(saveSessionNameButton, { button: 0 });
+    const searchButton = screen.getByText('Search');
+    expect(searchButton).toBeInTheDocument();
+    const restaurant = await screen.findByText('Overlook Family Restaurant - 1332 N Skidmore St Portland, OR 97217');
+    expect(restaurant).toBeInTheDocument();
+  });
+
+  it('should load new restaurants after clicking Search', async () => {
+    const restaurantDataZip = testRestaurantsByZip;
+    axios.get.mockImplementation(() => new Promise((resolve, reject) => {
+      resolve({
+        data: restaurantDataZip
+      });
+    }));
+
+    const sessionNameInput = screen.getByTestId('session-name-input');
+    fireEvent.change(sessionNameInput, { target: { value: 'Portland Dinner' } });
+    const saveSessionNameButton = screen.getByText('Save');
+    fireEvent.click(saveSessionNameButton, { button: 0 });
+    const searchInput = screen.getByTestId('restaurant-search-input');
+    fireEvent.change(searchInput, { target: { value: 'Bakeri' } });
+    const searchButton = screen.getByText('Search');
+    fireEvent.click(searchButton, { button: 0 });
+    expect(axios.get).toHaveBeenCalled();
+    const restaurant = await screen.findByText('Bakeri - 150 Wythe Ave Brooklyn, NY 11211');
+    expect(restaurant).toBeInTheDocument();
+  });
+
+  it('should load the next component after selecting a restaurant', async () => {
+    const restaurantDataZip = testRestaurantsByZip;
+    axios.get.mockImplementation(() => new Promise((resolve, reject) => {
+      resolve({
+        data: restaurantDataZip
+      });
+    }));
+
+    const sessionNameInput = screen.getByTestId('session-name-input');
+    fireEvent.change(sessionNameInput, { target: { value: 'Portland Dinner' } });
+    const saveSessionNameButton = screen.getByText('Save');
+    fireEvent.click(saveSessionNameButton, { button: 0 });
+    const searchInput = screen.getByTestId('restaurant-search-input');
+    fireEvent.change(searchInput, { target: { value: 'Bakeri' } });
+    const searchButton = screen.getByText('Search');
+    fireEvent.click(searchButton, { button: 0 });
+    expect(axios.get).toHaveBeenCalled();
+
+    const selectButtonsArray = screen.getAllByText('Select');
+    const selectButton = selectButtonsArray[0];
+    fireEvent.click(selectButton, { button: 0 });
+    const text = screen.getByText('Create your guest list');
+    expect(text).toBeInTheDocument();
   });
 });
